2675ebbc2576be08b1cd22db914e74c32bec8407
Rahul Gupta <2017chb1053@iitrpr.ac.in>
2021-09-08 12:55:13 +0530

    gen

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
+import nodeStatic from 'node-static'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const port = process.env.PORT || 5000
+
+const fileServer = new nodeStatic.Server()
 const app = express()
-const http = createServer(app)
+const http = createServer(app, function (req, res) {
+  fileServer.serve(req, res)
+}).listen(port)
 const io = new Server(http, {
   allowEIO3: true
 })
-
-const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, '/public')))
 
@@ -57,6 +61,6 @@ io.on('connection', (socket) => {
   })
 })
 
-http.listen(port, () => {
-  console.log(`listening on http://localhost:${port}`)
-})
+// http.listen(port, () => {
+//   console.log(`listening on http://localhost:${port}`)
+// })
